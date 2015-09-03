e084d90651d3c70750c880f814019cb87379a9df
Troy Trimble <troy.trimble@gmail.com>
2015-09-03 16:02:47 -0700

    update licence headers, add new c# code

--- csharp/src/ConsumerSurveysSample/Program.cs
+++ csharp/src/ConsumerSurveysSample/Program.cs
@@ -1,18 +1,37 @@
-﻿/*
- * Copyright 2015 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+﻿/* Copyright 2015 Google Inc. All Rights Reserved.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+/* Command-line tool for interacting with the Google Consumer Surveys API.
+To run, generate a client secret using https://console.developers.google.com/
+under the APIs and Auth Tab for your project. Then download the key.p12 file.
+
+For more instructions on how to obtain the local files necessary for OAuth
+authorization, please see https://github.com/google/consumer-surveys
+
+Use the following command line arguments.
+To create a survey:
+-o=create -oe=<email1> -se=<email>
+To set the number of desired responses on a survey:
+-o=set_num_responses -s=<id> -se=<email>
+To start the survey:
+-o=start -s=<id> -se=<email>
+To download survey results:
+-o=fetch -s=<id> -se=<email> -rf=result.xls
+To list the list of surveys you are an owner of:
+-o=list -se=<email>
+*/
 using System;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
@@ -27,21 +46,46 @@ namespace ConsumerSurveysSample
 {
     class Program
     {
+        // Constants that enumerate the various operations that the client allows.
+
+        // Create a new survey.
+        private static string CREATE = "create";
+
+        // Set the desired response count of an existing survey.
+        private static string SET_RESPONSE_COUNT = "set_response_count";
+
+        // Start an existing survey.
+        private static string START = "start";
+
+        // Fetch the result of an existing survey.
+        private static string FETCH = "fetch";
+
+        // List the surveys that this user owns.
+        private static string LIST = "list";
+
         static void Main(string[] args)
         {
+            List<String> operations = new List<string>();
+            operations.Add(CREATE);
+            operations.Add(SET_RESPONSE_COUNT);
+            operations.Add(START);
+            operations.Add(FETCH);
+            operations.Add(LIST);
+
             bool showHelp = false;
-            String serviceAccountEmail = null;
-            List<String> owners = new List<string>();
-            String completedSurveyId = null;
-            String resultFile = null;
-            String startSurveyId = null;
+            string serviceAccountEmail = null;
+            List<string> owners = new List<string>();
+            string resultFile = null;
+            string surveyId = null;
+            string operation = null;
             var p = new OptionSet() {
-                { "o|owner_email=", "Specify owners' email to use to create surveys.",
+                { "o|operation=",
+                  "The operation that you want to perform. Possible operations are: " + operations,
+                  v => operation = v },
+                { "s|survey_id=", "survey id to operate on.",
+                  v => surveyId = v },
+                { "oe|owner_email=", "List of survey owners' email to use to create surveys.",
                   v => owners.Add(v) },
-                { "c|completed_survey_id=", "survey id to retrieve excel results for.",
-                  v => completedSurveyId = v },
-                { "s|start_survey_id=", "survey id to start the survey.",
-                  v => startSurveyId = v },
                 { "r|result_file=", "filename to store excel results.",
                   v => resultFile = v },
                 { "se|service_account_email=", "Specify a service account email to use for auth.",
@@ -67,36 +111,83 @@ namespace ConsumerSurveysSample
                 return;
             }
 
-            if (completedSurveyId == null && owners.Count == 0 && startSurveyId == null)
-            {
-                Console.WriteLine("\n\nMissing at least one required argument: " +
-                                   "Please use owner_email or completed_survey_id flags.");
+            if (operation == null || !operations.Contains(operation)) {
+                Console.WriteLine("\nMissing or incorrect operation. " +
+                    "You must use one of these operations: " + operations);
                 return;
             }
+
+            if (serviceAccountEmail == null)
+            {
+                Console.WriteLine("\nserviceAccountEmail must be specified.");
+                return;
+            }
+
             var cs = GetServiceAccountCredential(serviceAccountEmail);
 
-            if (owners != null && owners.Count > 0)
+            if (cs == null)
             {
+                Console.WriteLine("\nA Consumer survey service was not created.");
+                return;
+            }
+
+            if (operation == CREATE)
+            {
+                if (owners == null || owners.Count == 0)
+                {
+                    Console.WriteLine("\nOwner must be specified when creating a survey.");
+                    return;
+                }
                 Survey survey = CreateSurvey(cs, owners);
-                String surveyId = survey.SurveyUrlId;
-                Console.WriteLine("Create survey with id: " + surveyId);
-                survey = UpdateSurveyResponseCount(cs, surveyId, 120);
+                Console.WriteLine("Create survey with id: " + survey.SurveyUrlId);
+                survey = UpdateSurveyResponseCount(cs, survey.SurveyUrlId, 120);
             }
-            if (startSurveyId != null)
+            if (operation == START)
             {
-                StartSurvey(cs, startSurveyId);
+                if (surveyId == null)
+                {
+                    Console.WriteLine("\nsurveyId must be specified when starting a survey.");
+                    return;
+                }
+                StartSurvey(cs, surveyId);
             }
-            if (completedSurveyId != null)
+            if (operation == SET_RESPONSE_COUNT)
             {
+                if (surveyId == null)
+                {
+                    Console.WriteLine("\nsurveyId must be specified when setting response count.");
+                    return;
+                }
+                UpdateSurveyResponseCount(cs, surveyId, 120);
+            }
+            if (operation == FETCH)
+            {
+                if (surveyId == null)
+                {
+                    Console.WriteLine("\nsurveyId must be specified for fetching results.");
+                    return;
+                }
                 if (resultFile == null)
                 {
                     resultFile = "results.xls";
                 }
-                GetSurveyResults(cs, completedSurveyId, resultFile);
+                GetSurveyResults(cs, surveyId, resultFile);
+            }
+            if (operation == LIST)
+            {
+                ListSurveys(cs);
             }
         }
 
-        // Creates a survey using ConsumersurveyService and sets the owners.
+        /// <summary>
+        /// Creates a new survey using a json object containing necessary survey fields.
+        /// </summary>
+        /// <param name="cs"> The consumer survey service used to send the HTTP requests.</param>
+        /// <param name="owners"> The list of owners that will be in the newly created survey.</param>
+        /// <returns>
+        /// A Survey object containing information about the survey.
+        /// </returns>
+
         private static Survey CreateSurvey(ConsumersurveysService cs, List<String> owners)
         {
             SurveyAudience audience = new SurveyAudience()
@@ -109,7 +200,7 @@ namespace ConsumerSurveysSample
                 UnitsPosition = "before",
                 Type = "openNumericQuestion",
                 Question = "How much did you pay for your last phone?",
-                //LowValueString = "1",
+                LowValueLabel = "1",
                 UnitOfMeasurementLabel = "$",
                 SingleLineResponse = true,
                 OpenTextPlaceholder = "enter amount here",
@@ -129,8 +220,17 @@ namespace ConsumerSurveysSample
             return createdSurvey;
         }
 
-        // Updates the response count of the survey.
-        private static Survey UpdateSurveyResponseCount(ConsumersurveysService cs, String surveyId, int responseCount)
+        /// <summary>
+        /// Updates the response count of the survey.
+        /// </summary>
+        /// <param name="cs"> The consumer survey service used to send the HTTP requests.</param>
+        /// <param name="surveyId"> The survey id for which we are updating the response count for.</param>
+        /// <param name="responseCount">  An integer specifing the new response count for the survey.</param>
+        /// <returns>
+        /// A Survey object containing information about the survey.
+        /// </returns>
+        private static Survey UpdateSurveyResponseCount(
+            ConsumersurveysService cs, String surveyId, int responseCount)
         {
             Survey survey = new Survey()
             {
@@ -141,7 +241,14 @@ namespace ConsumerSurveysSample
             return updatedSurvey;
         }
 
-        // Sends the survey to ops queue to start.
+        /// <summary>
+        /// Sends the survey to the review process and it is then started.
+        /// </summary>
+        /// <param name="cs"> The consumer survey service used to send the HTTP requests.</param>
+        /// <param name="surveyId"> The survey id of the survey we are starting.</param>
+        /// <returns>
+        /// A Survey object containing information about the survey.
+        /// </returns>      
         private static Survey StartSurvey(ConsumersurveysService cs, String surveyId)
         {
             Survey survey = new Survey()
@@ -152,7 +259,14 @@ namespace ConsumerSurveysSample
             return updatedSurvey;
         }
 
-        // Returns a survey given a survey id.
+        /// <summary>
+        /// Returns the Survey object for tbe specified survey id.
+        /// </summary>
+        /// <param name="cs"> The consumer survey service used to send the HTTP requests.</param>
+        /// <param name="surveyId"> The survey id of the Survey object we need.</param>
+        /// <returns>
+        /// A Survey object containing information about the survey.
+        /// </returns> 
         private static Survey GetSurvey(ConsumersurveysService cs, String surveyId)
         {
             Survey survey = cs.Surveys.Get(surveyId).Execute();
@@ -160,18 +274,52 @@ namespace ConsumerSurveysSample
             return survey;
         }
 
-        // Downloads the xls file containing the survey results.
-        private static void GetSurveyResults(ConsumersurveysService cs, String surveyId, String resultFile)
+        /// <summary>
+        /// Prints the surveys that are owned by the given user.
+        /// </summary>
+        /// <param name="cs"> The consumer survey service used to send the HTTP requests.</param>
+        private static void ListSurveys(ConsumersurveysService cs)
+        {
+            var surveyListResponse = cs.Surveys.List().Execute();
+            foreach (Survey survey in surveyListResponse.Resources)
+            {
+                Console.WriteLine(survey.SurveyUrlId);
+            }
+        }
+
+        /// <summary>
+        /// Writes the survey results into a xls file.
+        /// </summary>
+        /// <param name="cs"> The consumer survey service used to send the HTTP requests.</param>
+        /// <param name="surveyId"> The survey id for which we are downloading the results for.</param>
+        /// <param name="resultFile"> The file name which we write the survey results to.</param>
+        private static void GetSurveyResults(
+            ConsumersurveysService cs, String surveyId, String resultFile)
         {
             FileStream fileSteam = new FileStream(resultFile, FileMode.Create);
             cs.Results.Get(surveyId).Download(fileSteam);
         }
 
-        // Creates a Consumersurveys service that be used to send HTTP requests.
-        private static ConsumersurveysService GetServiceAccountCredential(String serviceAccountEmail)
+        /// <summary>
+        /// Creates a Consumersurveys service that be used to send HTTP requests.
+        /// </summary>
+        /// <param name="serviceAccointEmail"> The service account email we are using for 2LO.</param>
+        /// <returns>
+        /// The consumer survey service used to send the HTTP requests.
+        /// </returns>         
+        private static ConsumersurveysService GetServiceAccountCredential(
+            String serviceAccountEmail)
         {
-            var certificate = new X509Certificate2(@".\key.p12", "notasecret", X509KeyStorageFlags.Exportable);
-
+            X509Certificate2 certificate;
+            try {
+                certificate = new X509Certificate2(
+                    @".\key.p12", "notasecret", X509KeyStorageFlags.Exportable);
+            } catch (Exception e)
+            {
+                Console.WriteLine("Make sure key.p12 file is in the right location.\n" +
+                    e.ToString());
+                return null;
+            }
             ServiceAccountCredential credential = new ServiceAccountCredential(
                 new ServiceAccountCredential.Initializer(serviceAccountEmail)
                 {
