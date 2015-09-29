7139cd0f8bc81fd3a8f8a0a9016fedcc4ed1e9f3
Troy Trimble <troy.trimble@gmail.com>
2015-09-28 22:20:49 -0700

    add setup.py and requirements.txt for PyPi package building

---
+++ python/src/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""Packaging, distributing, and installing the ConsumerSurveys lib."""
+
+import setuptools
+
+# To debug, set DISTUTILS_DEBUG env var to anything.
+setuptools.setup(
+    name="GoogleConsumerSurveys",
+    version="0.0.0.1",
+    packages=setuptools.find_packages(),
+    author="Google Consumer Surveys",
+    author_email="gcs-api-trusted-testers@googlegroups.com",
+    keywords="google consumer surveys api client",
+    url="https://github.com/google/consumer-surveys",
+    license="Apache License 2.0",
+    description=("Client API for Google Consumer Surveys API"),
+    zip_safe=True,
+    include_package_data=True,
+    # Exclude these files from installation.
+    exclude_package_data={"": ["README"]},
+    install_requires=[
+      "google-api-python-client >= 1.4.2",
+      ],
+    extras_require={},
+)
